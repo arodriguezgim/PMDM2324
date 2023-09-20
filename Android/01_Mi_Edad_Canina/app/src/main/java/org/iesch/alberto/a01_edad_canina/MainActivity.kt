51cf9758342ca3547259e17126d7ec58e08651cf
arodriguezgim <arodriguezg@iesch.org>
2023-09-20 17:36:53 +0200

    Primera version

--- Android/01_Mi_Edad_Canina/app/src/main/java/org/iesch/alberto/a01_edad_canina/MainActivity.kt
+++ Android/01_Mi_Edad_Canina/app/src/main/java/org/iesch/alberto/a01_edad_canina/MainActivity.kt
@@ -2,10 +2,27 @@ package org.iesch.alberto.a01_edad_canina
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 1 - Tomamos el control sobre las Vistas que vamos a necesitar en la Activity
+        val resulText = findViewById<TextView>(R.id.result_text)
+        val calculateButton = findViewById<Button>(R.id.calculate_button)
+        val ageEdit = findViewById<EditText>(R.id.age_edit)
+
+        // 2 - Los botones tienen la propiedad onClickListener al pulsarlos
+        calculateButton.setOnClickListener {
+            val ageString = ageEdit.text.toString()
+            val ageInt = ageString.toInt()
+            val dogAge = ageInt * 7
+            resulText.text = "Si fuesras perro, tu edad sería de $dogAge años"
+
+        }
     }
 }
