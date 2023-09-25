e145da56bb32dbe2b9cab9b01c98fc4fc67291a0
arodriguezgim <arodriguezg@iesch.org>
2023-09-25 18:48:13 +0200

    Inicio App 02

---
+++ Android/02_Registro_de_superheroes/app/src/main/java/org/iesch/alberto/a02_registro_de_superheroes/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.iesch.alberto.a02_registro_de_superheroes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ Android/02_Registro_de_superheroes/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "02_Registro_de_superheroes"
+include(":app")
+ 
