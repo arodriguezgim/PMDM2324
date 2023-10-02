028e8a92e0531d2b891ea11c9f99ac28df4d9298
arodriguezgim <arodriguezg@iesch.org>
2023-10-02 19:29:54 +0200

    Pasando valores entre intents

--- Android/02_Registro_de_superheroes/app/src/main/java/org/iesch/alberto/a02_registro_de_superheroes/DetailActivity.kt
+++ Android/02_Registro_de_superheroes/app/src/main/java/org/iesch/alberto/a02_registro_de_superheroes/DetailActivity.kt
@@ -2,10 +2,35 @@ package org.iesch.alberto.a02_registro_de_superheroes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.iesch.alberto.a02_registro_de_superheroes.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+
+    // 13 (OPCIONAL) Podemos usar constantes para asignar llaves y NO equivocarnos
+    companion object{
+        const val SUPERHERO_NAME_KEY = "superhero_name"
+        const val ALTER_EGO_KEY = "alter_ego"
+        const val BIO_KEY = "bio"
+        const val POWER_KEY = "power"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        // 9 - añadimos el binding
+        val binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // 10 - Nos creamos una variable de tipo Bundle que recoge TODOS los valores tráido
+        val extras:Bundle = intent.extras!!
+        // 11 - Recogemos los valores traídos del Bundle
+        // Estos valores pueden ser nulos. Ponemos ?: para darles un valor por defcto en caso de que vengan nulos
+        val superHeroName = extras.getString(SUPERHERO_NAME_KEY)
+        val alterEgo = extras.getString(ALTER_EGO_KEY)
+        val bio = extras.getString(BIO_KEY)
+        val power = extras.getFloat(POWER_KEY)
+        // 12 - Mostramos los valores por pantalla
+        binding.heroName.text = superHeroName.toString()
+        binding.alterEgoText.text = alterEgo.toString()
+        binding.bioText.text = bio.toString()
+        binding.ratingBar.rating = power.toFloat()
+
     }
 }
--- Android/02_Registro_de_superheroes/app/src/main/java/org/iesch/alberto/a02_registro_de_superheroes/MainActivity.kt
+++ Android/02_Registro_de_superheroes/app/src/main/java/org/iesch/alberto/a02_registro_de_superheroes/MainActivity.kt
@@ -3,6 +3,7 @@ package org.iesch.alberto.a02_registro_de_superheroes
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import org.iesch.alberto.a02_registro_de_superheroes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,14 +14,46 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         // 2 - Comprobamos que tenemos el binding y llamamos a la función que abre el Activity de Detalle
         binding.saveButton.setOnClickListener {
-            abrirDetailActivity()
+            // 6 - Nos creamos las variables necesarias para pasárselas al intent
+            val superHeroName = binding.heroNameEdit.text.toString()
+            val alterEgo = binding.alterEgoEdit.text.toString()
+            val bio = binding.bioEdit.text.toString()
+            val power = binding.powerBar.rating
+            abrirDetailActivity(superHeroName, alterEgo, bio, power)
         }
     }
     // 3- esta función generará un Intent y nos llevará a detalle
-    private fun abrirDetailActivity() {
+    // 7
+    private fun abrirDetailActivity(superHeroName: String, alterEgo: String, bio:String, power: Float) {
         // 4 - El Intent debe tener la información de desde dónde se envía y hacia dónde quiero ir
         val intent = Intent(this, DetailActivity::class.java)
+        // 8 - Agregamos los valores al intent
+        intent.putExtra(DetailActivity.SUPERHERO_NAME_KEY, superHeroName)
+        intent.putExtra(DetailActivity.ALTER_EGO_KEY, alterEgo)
+        intent.putExtra(DetailActivity.BIO_KEY, bio)
+        intent.putExtra(DetailActivity.POWER_KEY, power)
+        Log.d("MainActivity", superHeroName)
         // 5 - Para utilizar el intent hemos de llamar a startActivity
         startActivity(intent)
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
