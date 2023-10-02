5e872c572defd5334ae5bfe8cad27e59eabaa831
arodriguezgim <arodriguezg@iesch.org>
2023-10-02 18:20:37 +0200

    Intent Explicito ok

--- Android/02_Registro_de_superheroes/app/src/main/java/org/iesch/alberto/a02_registro_de_superheroes/MainActivity.kt
+++ Android/02_Registro_de_superheroes/app/src/main/java/org/iesch/alberto/a02_registro_de_superheroes/MainActivity.kt
@@ -1,11 +1,26 @@
 package org.iesch.alberto.a02_registro_de_superheroes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.iesch.alberto.a02_registro_de_superheroes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // 1 - Aplicamos dataBinding
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // 2 - Comprobamos que tenemos el binding y llamamos a la función que abre el Activity de Detalle
+        binding.saveButton.setOnClickListener {
+            abrirDetailActivity()
+        }
+    }
+    // 3- esta función generará un Intent y nos llevará a detalle
+    private fun abrirDetailActivity() {
+        // 4 - El Intent debe tener la información de desde dónde se envía y hacia dónde quiero ir
+        val intent = Intent(this, DetailActivity::class.java)
+        // 5 - Para utilizar el intent hemos de llamar a startActivity
+        startActivity(intent)
     }
 }
