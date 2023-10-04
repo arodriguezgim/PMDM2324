fdc77d0d288692369198d31a39fa33b2b6b173cf
arodriguezgim <arodriguezg@iesch.org>
2023-10-04 18:13:56 +0200

    Ya echamos fotos

--- Android/02_Registro_de_superheroes/app/src/main/java/org/iesch/alberto/a02_registro_de_superheroes/DetailActivity.kt
+++ Android/02_Registro_de_superheroes/app/src/main/java/org/iesch/alberto/a02_registro_de_superheroes/DetailActivity.kt
@@ -1,5 +1,6 @@
 package org.iesch.alberto.a02_registro_de_superheroes
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.iesch.alberto.a02_registro_de_superheroes.databinding.ActivityDetailBinding
@@ -10,6 +11,8 @@ class DetailActivity : AppCompatActivity() {
     // 13 (OPCIONAL) Podemos usar constantes para asignar llaves y NO equivocarnos
     companion object{
         const val HERO_KEY = "superhero_name"
+        // 28 a
+        const val BITMAP_KEY = "bitmap_key"
         /*
         const val SUPERHERO_NAME_KEY = "superhero_name"
         const val ALTER_EGO_KEY = "alter_ego"
@@ -34,6 +37,8 @@ class DetailActivity : AppCompatActivity() {
          */
         // 20 - Recibimos el objeto Pacelable
         val superHero = extras.getParcelable<Hero>(HERO_KEY)!!
+        // 28 b
+        val bitmap = extras.getParcelable<Bitmap>(BITMAP_KEY)!!
         // 12 - Mostramos los valores por pantalla
         //21
         /*
@@ -42,6 +47,8 @@ class DetailActivity : AppCompatActivity() {
         binding.bioText.text = superHero.bio
         binding.ratingBar.rating = superHero.power
          */
+        //28 c
+        binding.imageView.setImageBitmap(bitmap)
         binding.superHero = superHero
 
     }
--- Android/02_Registro_de_superheroes/app/src/main/java/org/iesch/alberto/a02_registro_de_superheroes/MainActivity.kt
+++ Android/02_Registro_de_superheroes/app/src/main/java/org/iesch/alberto/a02_registro_de_superheroes/MainActivity.kt
@@ -1,18 +1,30 @@
 package org.iesch.alberto.a02_registro_de_superheroes
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
+import android.widget.ImageView
+import androidx.core.graphics.drawable.toBitmap
 import org.iesch.alberto.a02_registro_de_superheroes.databinding.ActivityMainBinding
 import org.iesch.alberto.a02_registro_de_superheroes.model.Hero
 
 class MainActivity : AppCompatActivity() {
+
+    // 27a - Nos creamos la variable global y la declaramos en el onCreate
+    private lateinit var heroImage: ImageView
+    // lateinit sirve para prometer a kotlin que cuando esta variable se establece ya estará declarada.
+    // Esta variable no puede ser nula
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // 1 - Aplicamos dataBinding
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //27 b
+        heroImage = binding.superheroImage
         // 2 - Comprobamos que tenemos el binding y llamamos a la función que abre el Activity de Detalle
         binding.saveButton.setOnClickListener {
             // 6 - Nos creamos las variables necesarias para pasárselas al intent
@@ -24,7 +36,20 @@ class MainActivity : AppCompatActivity() {
             val heroe = Hero(superHeroName, alterEgo, bio, power)
             abrirDetailActivity(heroe)
         }
+
+        //25 - Añadoimos el onclickListener a la Imagen
+        binding.superheroImage.setOnClickListener {
+            abrirCamara()
+        }
     }
+
+    private fun abrirCamara() {
+        // abrimos el INTENT IMPLICITO yq ue android es quien elige qué aplicacion abre este intent
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(cameraIntent, 1000)
+    }
+
+
     // 3- esta función generará un Intent y nos llevará a detalle
     // 7
     // 15 - Modificamos el metodo para que reciba solo un Hero
@@ -41,8 +66,23 @@ class MainActivity : AppCompatActivity() {
          */
         //18 - Pasamos solamente el superhero
         intent.putExtra(DetailActivity.HERO_KEY, heroe)
+        intent.putExtra(DetailActivity.BITMAP_KEY, heroImage.drawable.toBitmap())
         // 5 - Para utilizar el intent hemos de llamar a startActivity
         startActivity(intent)
+    }
+
+
+    //26 - onActivityResult es para recibir los datos de la camara
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if ( resultCode == Activity.RESULT_OK && requestCode == 1000){
+            val extras = data?.extras
+            val heroBitmap = extras?.getParcelable<Bitmap>("data")
+            // desde aqui no puedo aaceeder a heroImage. Hemos de crear la variable global
+            // 27 c
+            heroImage.setImageBitmap(heroBitmap)
+        }
+
     }
 }
 
