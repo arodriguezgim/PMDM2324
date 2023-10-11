4d547fc86ff325c2433df8702d496370c917d52b
arodriguezgim <arodriguezg@iesch.org>
2023-10-11 16:19:14 +0200

    App Ciclo de Vida Terminada

--- Android/03_Ciclo_de_Vida_Android/app/src/main/java/org/iesch/alberto/a03_ciclo_de_vida_android/MainActivity.kt
+++ Android/03_Ciclo_de_Vida_Android/app/src/main/java/org/iesch/alberto/a03_ciclo_de_vida_android/MainActivity.kt
@@ -2,10 +2,65 @@ package org.iesch.alberto.a03_ciclo_de_vida_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.i("CICLO_DE_VIDA_ANDROID","Entramos al método onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("CICLO_DE_VIDA_ANDROID","Entramos al método onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("CICLO_DE_VIDA_ANDROID","Entramos al método onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("CICLO_DE_VIDA_ANDROID","Entramos al método onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("CICLO_DE_VIDA_ANDROID","Entramos al método onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("CICLO_DE_VIDA_ANDROID","Entramos al método onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("CICLO_DE_VIDA_ANDROID","Entramos al método onRestart")
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
