330cd33da2cce216abf3997b4246bab8eae1cbdf
arodriguezgim <arodriguezg@iesch.org>
2023-10-16 17:41:31 +0200

    Proyecto 05 iniciado

---
+++ Android/05_Menu_DashBoard/app/src/main/java/org/iesch/alberto/a05_menu_dashboard/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.iesch.alberto.a05_menu_dashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
