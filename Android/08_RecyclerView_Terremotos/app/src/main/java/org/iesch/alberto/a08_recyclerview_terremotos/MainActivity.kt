43c31f3a5eb55235ebab59a5554391fe2945f2e2
arodriguezgim <arodriguezg@iesch.org>
2023-10-23 18:47:52 +0200

    Inicio APP 08

---
+++ Android/08_RecyclerView_Terremotos/app/src/main/java/org/iesch/alberto/a08_recyclerview_terremotos/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.iesch.alberto.a08_recyclerview_terremotos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
