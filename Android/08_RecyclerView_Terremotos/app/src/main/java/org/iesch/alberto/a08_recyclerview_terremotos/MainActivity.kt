948cf79421014f8d6b30ae6f4684b7bb7a7f1785
arodriguezgim <arodriguezg@iesch.org>
2023-10-25 17:55:09 +0200

    Añadido Empty Activitity

--- Android/08_RecyclerView_Terremotos/app/src/main/java/org/iesch/alberto/a08_recyclerview_terremotos/MainActivity.kt
+++ Android/08_RecyclerView_Terremotos/app/src/main/java/org/iesch/alberto/a08_recyclerview_terremotos/MainActivity.kt
@@ -2,6 +2,7 @@ package org.iesch.alberto.a08_recyclerview_terremotos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import org.iesch.alberto.a08_recyclerview_terremotos.adapter.TerremotoAdapter
 import org.iesch.alberto.a08_recyclerview_terremotos.databinding.ActivityMainBinding
@@ -17,19 +18,18 @@ class MainActivity : AppCompatActivity() {
         binding.eqRecycler.layoutManager = LinearLayoutManager(this)
 
         val listaTerremotos = mutableListOf<Terremoto>()
-        listaTerremotos.add(Terremoto("1","Madrid",4.5, 273846152L, -1.3343, 40.98786754))
-        listaTerremotos.add(Terremoto("2","Teruel",3.5, 273846152L, -1.3343, 40.98786754))
-        listaTerremotos.add(Terremoto("3","Murcia",7.3, 273846152L, -1.3343, 40.98786754))
-        listaTerremotos.add(Terremoto("4","Lorca",2.5, 273846152L, -1.3343, 40.98786754))
-        listaTerremotos.add(Terremoto("5","Castralvo",4.5, 273846152L, -1.3343, 40.98786754))
-        listaTerremotos.add(Terremoto("6","Algeciras",5.4, 273846152L, -1.3343, 40.98786754))
-        listaTerremotos.add(Terremoto("7","Lima",7.1, 273846152L, -1.3343, 40.98786754))
-        listaTerremotos.add(Terremoto("8","Los Angeles",4.5, 273846152L, -1.3343, 40.98786754))
+
 
 
         val adapter = TerremotoAdapter()
         binding.eqRecycler.adapter = adapter
         adapter.submitList(listaTerremotos)
+
+        if (listaTerremotos.isEmpty()){
+            binding.terremotoEmptyView.visibility = View.VISIBLE
+        } else {
+            binding.terremotoEmptyView.visibility = View.GONE
+        }
     }
 }
 
