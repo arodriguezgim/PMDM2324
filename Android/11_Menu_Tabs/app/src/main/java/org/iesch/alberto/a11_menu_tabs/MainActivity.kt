fbfd21fd00cd5d6a3afd23232273d56ac1643bcd
arodriguezgim <arodriguezg@iesch.org>
2023-11-06 19:29:45 +0100

    App casi terminada

--- Android/11_Menu_Tabs/app/src/main/java/org/iesch/alberto/a11_menu_tabs/MainActivity.kt
+++ Android/11_Menu_Tabs/app/src/main/java/org/iesch/alberto/a11_menu_tabs/MainActivity.kt
@@ -2,10 +2,13 @@ package org.iesch.alberto.a11_menu_tabs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.iesch.alberto.a11_menu_tabs.databinding.ActivityMainBinding
 
+private lateinit var binding : ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
