d4825775a2e93cfd98855c42538102bb15aab9ec
arodriguezgim <arodriguezg@iesch.org>
2023-11-08 18:01:17 +0100

    Inicio 13 SharedPreferences

--- Android/12_Corrutinas/app/src/main/java/org/iesch/alberto/a12_corrutinas/MainActivity.kt
+++ Android/12_Corrutinas/app/src/main/java/org/iesch/alberto/a12_corrutinas/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
         lifecycleScope.launch(Dispatchers.IO) {
             waitForCourutines()
             val response = retrofit.getSuperheroes("a")
