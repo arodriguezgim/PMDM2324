62eef4c6a47f54f62abac495267849cc67de782c
arodriguezgim <arodriguezg@iesch.org>
2023-11-08 16:23:18 +0100

    App Finalizada

--- Android/11_Menu_Tabs/app/build.gradle.kts
+++ Android/11_Menu_Tabs/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "org.iesch.alberto.a11_menu_tabs"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "org.iesch.alberto.a11_menu_tabs"
--- Android/11_Menu_Tabs/app/src/main/java/org/iesch/alberto/a11_menu_tabs/MainActivity.kt
+++ Android/11_Menu_Tabs/app/src/main/java/org/iesch/alberto/a11_menu_tabs/MainActivity.kt
@@ -2,6 +2,7 @@ package org.iesch.alberto.a11_menu_tabs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import org.iesch.alberto.a11_menu_tabs.databinding.ActivityMainBinding
 
 private lateinit var binding : ActivityMainBinding
@@ -10,5 +11,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.bnv.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.settings -> replaceFragment(SettingsFragment())
+                R.id.profile -> replaceFragment(ProfileFragment())
+                else -> {}
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fg : Fragment) {
+        val fm = supportFragmentManager
+        val fTransaction = fm.beginTransaction()
+        fTransaction.replace(R.id.framelayout, fg)
+        fTransaction.commit()
     }
 }
