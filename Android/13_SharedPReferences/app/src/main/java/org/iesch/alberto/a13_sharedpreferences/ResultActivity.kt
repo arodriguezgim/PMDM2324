056bab11b3788696c1fa98770411a32fdcf38e26
arodriguezgim <arodriguezg@iesch.org>
2023-11-08 18:13:16 +0100

    xml terminado

---
+++ Android/13_SharedPReferences/app/src/main/java/org/iesch/alberto/a13_sharedpreferences/ResultActivity.kt
@@ -0,0 +1,11 @@
+package org.iesch.alberto.a13_sharedpreferences
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
