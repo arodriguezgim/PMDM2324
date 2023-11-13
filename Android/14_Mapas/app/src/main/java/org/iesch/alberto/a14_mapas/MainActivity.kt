7322e45b868104b6db8b58c9be76db263e3a57ab
arodriguezgim <arodriguezg@iesch.org>
2023-11-13 19:11:23 +0100

    App Mapas Iniciada

---
+++ Android/14_Mapas/app/src/main/java/org/iesch/alberto/a14_mapas/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.iesch.alberto.a14_mapas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
