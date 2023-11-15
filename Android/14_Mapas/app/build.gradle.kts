f377a5e10847bdd7a11ac334846294a572eef2f2
arodriguezgim <arodriguezg@iesch.org>
2023-11-15 16:25:01 +0100

    Cargamos el mapa

--- Android/14_Mapas/app/build.gradle.kts
+++ Android/14_Mapas/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "org.iesch.alberto.a14_mapas"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "org.iesch.alberto.a14_mapas"
@@ -41,6 +41,9 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    // MApas de Google
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- Android/14_Mapas/app/src/main/java/org/iesch/alberto/a14_mapas/MainActivity.kt
+++ Android/14_Mapas/app/src/main/java/org/iesch/alberto/a14_mapas/MainActivity.kt
@@ -2,10 +2,48 @@ package org.iesch.alberto.a14_mapas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var map : GoogleMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createFragment()
+    }
+
+    private fun createFragment() {
+        // Este método nos creará el mapa
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        // Este metodo se llamará cuando el mapa haya sido cargado
+        map = googleMap
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
