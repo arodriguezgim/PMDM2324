03e0214adc428f8b7dca5c44f9d6684ff2fe470a
arodriguezgim <arodriguezg@iesch.org>
2023-11-22 17:12:17 +0100

    Registro Firebase ok y SharedPreferences

--- Android/15_Firebase_App/app/src/main/java/org/iesch/alberto/firebase/HomeActivity.kt
+++ Android/15_Firebase_App/app/src/main/java/org/iesch/alberto/firebase/HomeActivity.kt
@@ -1,11 +1,85 @@
 package org.iesch.alberto.firebase
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
+import com.google.firebase.auth.FirebaseAuth
+import org.iesch.alberto.firebase.databinding.ActivityHomeBinding
 
+enum class ProviderType {
+    BASIC,
+}
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // Recuperar los datos
+        val bundle = intent.extras
+        val email = bundle?.getString("email")
+        val provider = bundle?.getString("provider")
+        setup(email ?: "", provider ?: "")
+
+        // Guardado de datos con SharedPreferences
+        val prefs = getSharedPreferences("preferencias", Context.MODE_PRIVATE)
+        prefs.edit().putString("email", email).putString("provider", provider).apply()
+    }
+
+    private fun setup(email: String, provider: String) {
+        title = "Home"
+        binding.tvEmail.text = email.toString()
+        binding.tvProvider.text = provider.toString()
+
+        //BOTON CERRAR SESION
+        binding.btnCerrarSesion.setOnClickListener {
+            // Borrado de datos
+            val prefs = getSharedPreferences("preferencias", Context.MODE_PRIVATE)
+            prefs.edit().clear().apply()
+
+            FirebaseAuth.getInstance().signOut()
+            finish()
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Android/15_Firebase_App/app/src/main/java/org/iesch/alberto/firebase/LoginActivity.kt
+++ Android/15_Firebase_App/app/src/main/java/org/iesch/alberto/firebase/LoginActivity.kt
@@ -1,13 +1,22 @@
 package org.iesch.alberto.firebase
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.FirebaseAuth
 import org.iesch.alberto.firebase.databinding.ActivityLoginBinding
 
 private lateinit var binding: ActivityLoginBinding
 class LoginActivity : AppCompatActivity() {
+
+    override fun onStart() {
+        super.onStart()
+        binding.authLayout.visibility = View.VISIBLE
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         //Cambiar de tema
         Thread.sleep(3000)
@@ -24,6 +33,19 @@ class LoginActivity : AppCompatActivity() {
         analytics.logEvent("InicioScreen",bundle )
         //Autenticacion
         setup()
+        // Sesion
+        session()
+    }
+
+    private fun session() {
+        val prefs = getSharedPreferences("preferencias", Context.MODE_PRIVATE)
+        val email = prefs.getString("email", null)
+        val provider = prefs.getString("provider", null)
+
+        if (email != null && provider != null){
+            binding.authLayout.visibility = View.INVISIBLE
+            showHome(email, ProviderType.valueOf(provider))
+        }
     }
 
     private fun setup() {
@@ -40,14 +62,57 @@ class LoginActivity : AppCompatActivity() {
                 // Le añadimos unlistener para comprobar si se ha registrado completamente el usuario
                     .addOnCompleteListener {
                         if (it.isSuccessful){
-                            // Si se ha añadido el usuario pasamos a Hoome
+                            // Si se ha añadido el usuario pasamos a Home
+                            showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
                         } else {
                             // Error
-                            
+                            showAlert()
                         }
                     }
             }
         }
+
+        // PULSAR EL BOTON LOGUEAR
+        binding.btnLogin.setOnClickListener {
+            //comprobamos que email y contraseña no estén vacíos
+            if ( binding.etEmail.text.isNotEmpty() && binding.etPassword.text.isNotEmpty()) {
+                // Iniciamos el LOGIN de usuario en Firebase
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                    binding.etEmail.text.toString(),
+                    binding.etPassword.text.toString()
+                )
+                    // Le añadimos unlistener para comprobar si se ha registrado completamente el usuario
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            // Si se ha añadido el usuario pasamos a Home
+                            showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                        } else {
+                            // Error
+                            showAlert()
+                        }
+                    }
+            }
+        }
+
+
+
+    }
+
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error al autenticar al usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showHome(email : String, provider : ProviderType) {
+        val homeIntent = Intent(this, HomeActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(homeIntent)
     }
 }
 
