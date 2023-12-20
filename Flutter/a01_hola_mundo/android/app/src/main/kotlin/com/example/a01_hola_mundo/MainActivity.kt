7679778179bad34ff116c75db26d2db0f4ec39f0
arodriguezgim <arodriguezg@iesch.org>
2023-12-20 18:25:57 +0100

    Flutter comenzado

---
+++ Flutter/a01_hola_mundo/android/app/src/main/kotlin/com/example/a01_hola_mundo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.a01_hola_mundo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Flutter/a01_hola_mundo/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp( MyApp() );
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Hola a DAM 2');
+  }
+
+}
