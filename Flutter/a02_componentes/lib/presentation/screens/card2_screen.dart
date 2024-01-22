e6f9cbb8421ee1c760cf3c1ab08625d08f043e34
arodriguezgim <arodriguezg@iesch.org>
2024-01-22 19:31:55 +0100

    Fin clase Lunes

--- Flutter/a02_componentes/lib/presentation/screens/card2_screen.dart
+++ Flutter/a02_componentes/lib/presentation/screens/card2_screen.dart
@@ -14,6 +14,14 @@ class Card2Screen extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
         children: const [
           CardPersonalizada1(),
+          SizedBox(height: 10),
+          CardPersonalizada2( urlImage: 'https://switzerland-tour.com/storage/media/4-ForArticles/swiss-mountains.jpg'),
+          SizedBox(height: 10),
+          CardPersonalizada2( urlImage: 'https://static.vecteezy.com/system/resources/previews/003/623/626/non_2x/sunset-lake-landscape-illustration-free-vector.jpg',),
+          SizedBox(height: 10),
+          CardPersonalizada2( urlImage: 'https://i.pinimg.com/originals/28/25/e3/2825e3e901b7ede50931cbbf751a4c3e.jpg', comentario: 'Un bonito Paisaje'),
+          SizedBox(height: 10),
+          
 
 
         ],
--- Flutter/a02_componentes/lib/presentation/widgets/card_personalizada2.dart
+++ Flutter/a02_componentes/lib/presentation/widgets/card_personalizada2.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
 
 class CardPersonalizada2 extends StatelessWidget {
-  const CardPersonalizada2({super.key});
+
+
+  const CardPersonalizada2({super.key, required this.urlImage, this.comentario});
+
+  final String urlImage;
+  final String? comentario;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
+      elevation: 10,
+      child: Column(
+        children: [
+          FadeInImage(
+            width: double.infinity,
+            height: 230,
+            placeholder: const AssetImage('assets/jar-loading.gif'),
+            image: NetworkImage(urlImage),
+            fit: BoxFit.cover,
+            fadeInDuration: const Duration(milliseconds: 800),
+            ),
+
+            if ( comentario != null )
+            Container(
+              alignment: Alignment.centerRight,
+              padding: const EdgeInsets.all(10),
+              child: Text( comentario!  ),
+              )
+        ],
+      ),
+    );
   }
 }
