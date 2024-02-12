198157e72c185de004759bd8a5dfdf6bcefb0366
arodriguezgim <arodriguezg@iesch.org>
2024-02-12 19:26:41 +0100

    API Terminado

--- Flutter/a03_menu_dash/lib/presentation/screens/sub_screens/pets_pages/pets_screen.dart
+++ Flutter/a03_menu_dash/lib/presentation/screens/sub_screens/pets_pages/pets_screen.dart
@@ -1,16 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:menu_dash/entities/model/pets_model.dart';
+import 'package:menu_dash/presentation/provider/pets_provider.dart';
+import 'package:provider/provider.dart';
 
-class PetsScreen extends StatelessWidget {
+class PetsScreen extends StatefulWidget {
    
   const PetsScreen({Key? key}) : super(key: key);
-  
+
+  @override
+  State<PetsScreen> createState() => _PetsScreenState();
+}
+
+
+class _PetsScreenState extends State<PetsScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    final provider = Provider.of<PetsProvider>(context, listen: false);
+    provider.getDataFromAPI();
+    print('Llamada a la API');
+  }
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
-      body: Center(
-         child: Text('PetsScreen'),
+
+    final provider = Provider.of<PetsProvider>(context);
+    print('Llamada a la API desde build');
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('API Perretes'), elevation: 0,),
+      body: provider.isLoading
+              ? mostrarLoadingUI()
+              : provider.error.isNotEmpty
+                  ? mostrarErrorUI( provider.error )
+                  : mostrarBodyUI( provider.pets ),
+    );
+  }
+
+  mostrarLoadingUI() {
+    return const Center(
+      child: CircularProgressIndicator.adaptive(),
+    );
+  }
+
+  mostrarBodyUI(PetsResponse pets) {
+    return ListView.builder(
+      itemCount: pets.data.length,
+      itemBuilder:  (context, index) => ListTile(
+        leading: CircleAvatar(
+          radius: 22,
+          backgroundImage: NetworkImage(pets.data[index].petImage),
+          backgroundColor: Colors.white,
+        ),
+        title: Text(pets.data[index].petName),
+        trailing: pets.data[index].isFriendly
+                    ? const SizedBox()
+                    : const Icon(Icons.pets, color: Colors.red,),
       ),
+      );
+  }
+
+  mostrarErrorUI(String error) {
+    return Center(
+      child: Text(error, style: const TextStyle(color: Colors.red),),
     );
   }
 }
